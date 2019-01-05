dc1e1d8f06620d451447de2a760b86ade7100a8f
Linden Chiu <lindentree@Lindens-MacBook-Pro.local>
2019-01-04 23:13:02 -0800

    begin ready for deploy

--- proxy/server.js
+++ proxy/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
-const path = require('path');
+const proxy = require('http-proxy-middleware');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
+app.use(morgan('tiny'));
+app.use('/watches/:wid', express.static('public'));
 
+app.use('/api/watches/:wid/photos', proxy({ target: 'http://54.211.73.211', changeOrigin: true }));
+app.use('/api/watches/:wid/summary', proxy({ target: 'http://54.87.165.253', changeOrigin: true }));
+app.use('/api/watches/:wid/details', proxy({ target: 'http://13.57.38.154', changeOrigin: true }));
+app.use('/api/watches/:wid/reviews', proxy({ target: 'http://18.218.241.2', changeOrigin: true }));
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('listening on port 3000...');
 });
